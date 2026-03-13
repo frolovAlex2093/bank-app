20ea8289b597c523ce4226eb492abc7093c8f3cb
frolov.alex <243@elpts.ru>
2026-03-13 09:35:47 +0300

    add transfer-service notifications-service

---
+++ notifications-service/src/test/java/ru/yandex/practicum/notificationsservice/NotificationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.notificationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
