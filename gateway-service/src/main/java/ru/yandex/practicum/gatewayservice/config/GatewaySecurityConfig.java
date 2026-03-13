8891bc51ff9f631f965401b372f851d07c576dd9
frolov.alex <243@elpts.ru>
2026-03-13 13:43:07 +0300

    edit gateway

---
+++ gateway-service/src/main/java/ru/yandex/practicum/gatewayservice/config/GatewaySecurityConfig.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login/**", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/", true));
+        return http.build();
+    }
+}
