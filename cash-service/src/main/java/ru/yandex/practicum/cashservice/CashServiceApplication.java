42c6b3706530c22c50a7aee5f5b6ad9a344bc74b
frolov.alex <243@elpts.ru>
2026-03-13 09:28:58 +0300

    add cash-service

---
+++ cash-service/src/main/java/ru/yandex/practicum/cashservice/CashServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.cashservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashServiceApplication.class, args);
+    }
+
+}
