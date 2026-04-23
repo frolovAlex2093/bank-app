fed758b3def97c1221605e41b27c9c8b37ff144c
frolov.alex <243@elpts.ru>
2026-04-23 19:01:28 +0300

    add kafka Listener

--- notifications-service/src/main/java/ru/yandex/practicum/notificationsservice/dto/NotificationRequest.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.notificationsservice.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record NotificationRequest(
-        @NotBlank String login,
-        @NotBlank String message
-) {}
